59e4699ed977933311d15ea1d7ed8006ca181654
mattdesl <dave.des@gmail.com>
2014-11-29 17:25:39 -0500

    new build

--- build.js
+++ build.js
@@ -2,18 +2,27 @@ var path = require('path')
 var fs = require('fs')
 var minstache = require('minstache')
 var datauri = require('datauri')
-var minify = require('html-minifier').minify
+var minify = require('html-minify').minify
 
 var html = fs.readFileSync(__dirname+'/dev.html', 'utf8')
 
 
 var style = 'style/main.css'
-var img = 'img/spinner.png'
-var b64 = datauri(path.join(__dirname, img))
 
 html = html.replace(style, 'style/main.min.css')
-html = html.replace(img, b64)
+html = inline(html, 'img/spinner.png')
+html = inline(html, 'img/powered-by-google-on-white@2x.png')
 html = html.replace('{{entry}}', 'build/bundle.js')
-// console.log(minify(html))
+
+console.log(minify(html, {
+    removeComments: true,
+    collapseWhitespace: true,
+    minifyJS: true,
+    minifyCSS: true
+}))
 
 fs.writeFileSync(__dirname+'/index.html', html)
+
+function inline(html, img) {
+    return html.replace(img, datauri(path.join(__dirname, img)))
+}
